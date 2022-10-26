32e84f71a2fed3802186222c02f7834edc2d696e
tamddk <siapaa.akuhh@gmail.com>
2022-10-26 18:03:43 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,26 @@ export default function Home() {
         </p>
       </main> */}
       <main>
-        <div className="container">
-          <div className="row">
-            <div className="col">LEFT</div>
-            <div className="col">RIGHT</div>
+        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+          <div className="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div className="my-3 py-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-light shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 p-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-dark shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
           </div>
         </div>
       </main>
