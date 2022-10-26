7ecd6246defc634718f2fff69d50f47e86ff3a52
tamddk <siapaa.akuhh@gmail.com>
2022-10-26 17:52:51 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,19 @@ export default function Home() {
         <title>Portofolio - Sodik</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
+      {/* <main>
+        <Header title="Welcome to my portfolio" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+      </main> */}
+      <main>
+        <div className="container">
+          <div className="row">
+            <div className="col">LEFT</div>
+            <div className="col">RIGHT</div>
+          </div>
+        </div>
       </main>
 
       <Footer />
