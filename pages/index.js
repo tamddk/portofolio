5b77184722d48c1ea69c0b772025d59ed31409ee
tamddk <siapaa.akuhh@gmail.com>
2022-10-26 17:58:27 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,12 @@ export default function Home() {
         </div>
       </main>
 
-      <Footer>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossorigin="anonymous"
-        ></script>
-      </Footer>
+      <Footer />
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossOrigin="anonymous"
+      ></script>
     </div>
   );
 }
